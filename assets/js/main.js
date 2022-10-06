42b163387e50e4b347892cea1091908fb5ed397f
thanhlongthuyet <qzarsixx@gmail.com>
2022-10-06 22:39:31 +0700

    update stick carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,24 @@
 $(".carousel").carousel({
-  interval: 2500,
+    interval: 2500,
 });
 
 $(".slick-slider").slick({
-  slidesToShow: 1,
-  autoplay: true,
-  dots: false,
-  arrows: false,
-  infinite: true,
+    slidesToShow: 1,
+    autoplay: true,
+    dots: false,
+    arrows: false,
+    infinite: true,
+    vertical: true,
+    draggable: true,
+    speed: 600,
+    autoplaySpeed: 2500,
+});
+$(".slick-slider").on("wheel", function (e) {
+    e.preventDefault();
+    let scrollY = e.originalEvent.deltaY;
+    if (scrollY < 0) {
+        $(this).slick("slickNext");
+    } else {
+        $(this).slick("slickPrev");
+    }
 });
