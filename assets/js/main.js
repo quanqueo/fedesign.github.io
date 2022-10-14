96a7036e7db358c0746823582b0197d2e23961dc
quanqueo <quantao134@gmail.com>
2022-10-14 15:32:25 +0700

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,48 @@
-$(".carousel").carousel({
-    interval: 2500,
-});
+$(document).ready(function(){
+        $(".carousel").carousel({
+        interval: 2500,
+    });
 
-$(".slick-slider").slick({
-    slidesToShow: 1,
-    autoplay: true,
-    dots: false,
-    arrows: false,
-    infinite: false,
-    vertical: true,
-    draggable: true,
-    speed: 300,
-    autoplaySpeed: 2000,
-});
+    $(".slick-slider").slick({
+        slidesToShow: 1,
+        autoplay: true,
+        dots: false,
+        arrows: false,
+        infinite: false,
+        vertical: true,
+        draggable: true,
+        speed: 300,
+        autoplaySpeed: 2000,
+    });
 
-$(".slick-slider").on("wheel", function (e)  {
+    $(".slick-slider").on("wheel", function (e)  {
 
-    var item_length = $(".slick-slider .stick-carousel-item").length - 1;
-    
-    let scrollY = e.originalEvent.deltaY;
-    if (scrollY < 0) {
-        if (!($(this).slick("slickCurrentSlide") == 0)) {
-            e.preventDefault();
-            $(this).slick("slickPrev");
+        var item_length = $(".slick-slider .stick-carousel-item").length - 1;
+        
+        let scrollY = e.originalEvent.deltaY;
+        if (scrollY < 0) {
+            if (!($(this).slick("slickCurrentSlide") == 0)) {
+                e.preventDefault();
+                $(this).slick("slickPrev");
+            }
+        } else {
+            if (!($(this).slick("slickCurrentSlide") == item_length)) {
+                e.preventDefault();
+                $(this).slick("slickNext");
+            }
         }
-    } else {
-        if (!($(this).slick("slickCurrentSlide") == item_length)) {
-            e.preventDefault();
-            $(this).slick("slickNext");
+    });
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() >= 20){
+            $("#header").css({
+                "background-color": "white",
+            });
         }
-    }
+        else{
+            $("#header").css({
+                "background-color": "transparent",
+            });;
+        }
+    });
 });
