05319638bbdbaaa9d9785bf9bc6d8b2bcadbb6a0
SThuan13 <vm.sturdy@gmail.com>
2022-10-21 22:14:59 +0700

    Carousel: Done!, detail: 4 items hoạt đông chưa có khóa học

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,40 +18,40 @@ $(document).ready(function(){
         
     });
 
-    $(".carousel").carousel({
+    $("#carousel").carousel({
         interval: 2500,
 
     });
 
-    $(".slick-slider").slick({
-        slidesToShow: 1,
-        autoplay: true,
-        dots: false,
-        arrows: false,
-        infinite: false,
-        vertical: true,
-        draggable: true,
-        speed: 300,
-        autoplaySpeed: 2000,
-    });
+    // $(".slick-slider").slick({
+    //     slidesToShow: 1,
+    //     autoplay: true,
+    //     dots: false,
+    //     arrows: false,
+    //     infinite: false,
+    //     vertical: true,
+    //     draggable: true,
+    //     speed: 300,
+    //     autoplaySpeed: 2000,
+    // });
 
-    $(".slick-slider").on("wheel", function (e)  {
+    // $(".slick-slider").on("wheel", function (e)  {
 
-        var item_length = $(".slick-slider .stick-carousel-item").length - 1;
+    //     var item_length = $(".slick-slider .stick-carousel-item").length - 1;
         
-        let scrollY = e.originalEvent.deltaY;
-        if (scrollY < 0) {
-            if (!($(this).slick("slickCurrentSlide") == 0)) {
-                e.preventDefault();
-                $(this).slick("slickPrev");
-            }
-        } else {
-            if (!($(this).slick("slickCurrentSlide") == item_length)) {
-                e.preventDefault();
-                $(this).slick("slickNext");
-            }
-        }
-    });
+    //     let scrollY = e.originalEvent.deltaY;
+    //     if (scrollY < 0) {
+    //         if (!($(this).slick("slickCurrentSlide") == 0)) {
+    //             e.preventDefault();
+    //             $(this).slick("slickPrev");
+    //         }
+    //     } else {
+    //         if (!($(this).slick("slickCurrentSlide") == item_length)) {
+    //             e.preventDefault();
+    //             $(this).slick("slickNext");
+    //         }
+    //     }
+    // });
 
     $("#backtop").click(function() {
         $("html, body").animate({ scrollTop: 0 }, 600);
