667205474f3dfec501695fa3b7eb53c8d40a21cb
thanhlongthuyet <qzarsixx@gmail.com>
2022-10-21 21:36:52 +0700

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,36 +23,6 @@ $(document).ready(function(){
 
     });
 
-    $(".slick-slider").slick({
-        slidesToShow: 1,
-        autoplay: true,
-        dots: false,
-        arrows: false,
-        infinite: false,
-        vertical: true,
-        draggable: true,
-        speed: 300,
-        autoplaySpeed: 2000,
-    });
-
-    $(".slick-slider").on("wheel", function (e)  {
-
-        var item_length = $(".slick-slider .stick-carousel-item").length - 1;
-        
-        let scrollY = e.originalEvent.deltaY;
-        if (scrollY < 0) {
-            if (!($(this).slick("slickCurrentSlide") == 0)) {
-                e.preventDefault();
-                $(this).slick("slickPrev");
-            }
-        } else {
-            if (!($(this).slick("slickCurrentSlide") == item_length)) {
-                e.preventDefault();
-                $(this).slick("slickNext");
-            }
-        }
-    });
-
     $("#backtop").click(function() {
         $("html, body").animate({ scrollTop: 0 }, 600);
     });
